0e311c9a84e4a2e70b92a55395d999e16f8de60a
Joseph Marbella <dieptuber@gmail.com>
2024-02-17 01:59:57 -0700

    feat: check if program active

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 import time
+import constants
+import win32gui
 
 
 def get_timestamp(start_time: float = time.perf_counter()) -> tuple[int, int, int]:
@@ -14,3 +16,10 @@ def terminate_program(start_time: float):
     seconds, minutes, hours = get_timestamp(start_time)
     print(f"AFK farm session lasted for {hours}h {minutes}m {seconds}s")
     os._exit(0)
+
+
+def check_granblue_relink():
+    hwnd = win32gui.FindWindow(None, constants.HWND_NAME)
+    if hwnd == 0:
+        print("Granblue Fantasy: Relink must be running to execute this script")
+        os._exit(0)
