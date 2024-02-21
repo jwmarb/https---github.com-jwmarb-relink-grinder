a5ebb74f4a84a5f6951ae0dc5e8ea59d19d063c8
Joseph Marbella <dieptuber@gmail.com>
2024-02-20 23:55:33 -0700

    feat: automatic knickknack refill

--- src/constants.py
+++ src/constants.py
@@ -4,7 +4,12 @@ REPLAY_QUEST = "images/reviewing_quest.png"
 HP_ZERO = "images/hp_zero.png"
 INSUFFICIENT_KNICKKNACKS = "images/insufficient_knickknacks.png"
 REPEAT_QUEST_FIRST_PROMPT = "images/repeat_quest_first_prompt.png"
+KNICKKNACK_VOUCHERS = "images/knickknack_vouchers.png"
+TRADE_SIGILS = "images/trade_sigils.png"
+SELECT_TRANSMUTATION = "images/select_transmutation.png"
+TRADE_ALL = "images/trade_all.png"
+TRADE_WARNING = "images/trade_warning.png"
 
 HWND_NAME = "Granblue Fantasy: Relink"
 CONFIDENCE = 0.7
-INPUT_DELAY = 0.001
+INPUT_DELAY = 0.025
--- src/transmute_sigils.py
+++ src/transmute_sigils.py
@@ -1,3 +1,4 @@
+import time
 import constants
 import keyboard
 import threading
@@ -12,6 +13,7 @@ def exit_listener(_):
 
 def main():
     paused = False
+    DELAY = 0.25
 
     def pause_listener(_):
         nonlocal paused
@@ -28,9 +30,89 @@ def main():
 
     keyboard.on_press_key(callback=exit_listener, key="escape", suppress=True)
 
-    while not is_on_screen(constants.INSUFFICIENT_KNICKKNACKS):
-        if not paused:
-            Macros.left_click()
+    while True:
+        while not is_on_screen(constants.INSUFFICIENT_KNICKKNACKS):
+            if not paused:
+                Macros.xbox_a()
+
+        # User navigates back to transmute sigils screen
+        while not is_on_screen(constants.SELECT_TRANSMUTATION, c=0.99):
+            Macros.xbox_a()
+            time.sleep(0.5)
+
+        time.sleep(1)
+
+        # Back to main navigation
+        Macros.xbox_b()
+        time.sleep(DELAY)
+
+        # Highlights "Knickknack Vouchers"
+        Macros.xbox_dpad_up()
+        time.sleep(DELAY)
+
+        # Navigates into "Knickknack Vouchers"
+        Macros.xbox_a()
+        time.sleep(DELAY)
+
+        # Navigates into "Trade Sigils"
+        Macros.xbox_a()
+        time.sleep(DELAY)
+
+        # Opens "Sort & Filter/Trade All"
+        Macros.xbox_back()
+        time.sleep(DELAY)
+
+        # "Trade All Using Specified Criteria"
+        Macros.xbox_x()
+        time.sleep(1)
+
+        if not is_on_screen(constants.TRADE_ALL):
+            print("You have no more sigils to trade for")
+            os._exit(0)
+
+        # Moves from "Cancel" to "Trade"
+        Macros.xbox_dpad_up()
+        time.sleep(DELAY)
+
+        # Navigates to "Trade"
+        Macros.xbox_a()
+        time.sleep(1)
+
+        # Ticks "Trade" button since the trade includes high rarity Sigils
+        Macros.xbox_a()
+        time.sleep(DELAY)
+
+        # Navigates to unlocked "Trade" button
+        Macros.xbox_dpad_down()
+        time.sleep(DELAY)
+
+        # Confirms action
+        Macros.xbox_a()
+        time.sleep(DELAY)
+
+        # Exits out of "Thank You!"
+        Macros.xbox_a()
+        time.sleep(0.5)
+
+        # Goes back to Knickknack Vouchers menu
+        Macros.xbox_b()
+        time.sleep(DELAY)
+
+        # Goes back to Main screen (Siero's Knickknack Shack)
+        Macros.xbox_b()
+        time.sleep(DELAY)
+
+        # Navigates to "Transmute Sigils"
+        Macros.xbox_dpad_down()
+        time.sleep(DELAY)
+
+        # Confirms navigation
+        Macros.xbox_a()
+        time.sleep(DELAY)
+
+        # At this point, the process starts again until the user desires to 
+        # end the script
+
 
 
 if __name__ == "__main__":
