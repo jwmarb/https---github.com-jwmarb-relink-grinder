2d50a4d542f6d3abffcfda03c3368fc17b9b0d35
Joseph Marbella <dieptuber@gmail.com>
2024-03-08 12:11:20 -0700

    feat: move outside function

--- src/transmute_sigils.py
+++ src/transmute_sigils.py
@@ -6,14 +6,92 @@ import os
 from macros import Macros
 from utils import is_on_screen
 
+DELAY = 0.25
 
 def exit_listener(_):
     os._exit(0)
 
+def sell_sigils():
+    # User navigates back to transmute sigils screen
+    while not is_on_screen(constants.SELECT_TRANSMUTATION, c=0.99):
+        Macros.xbox_a()
+        time.sleep(0.5)
+
+    time.sleep(1)
+
+    # Back to main navigation
+    Macros.xbox_b()
+    time.sleep(DELAY)
+
+    # Highlights "Knickknack Vouchers"
+    Macros.xbox_dpad_up()
+    time.sleep(DELAY)
+
+    # Navigates into "Knickknack Vouchers"
+    Macros.xbox_a()
+    time.sleep(DELAY)
+
+    # Navigates into "Trade Sigils"
+    Macros.xbox_a()
+    time.sleep(DELAY)
+
+    # Opens "Sort & Filter/Trade All"
+    Macros.xbox_back()
+    time.sleep(DELAY)
+
+    # "Trade All Using Specified Criteria"
+    Macros.xbox_x()
+    time.sleep(1)
+
+    if not is_on_screen(constants.TRADE_ALL):
+        print("You have no more sigils to trade for")
+        os._exit(0)
+
+    # Moves from "Cancel" to "Trade"
+    Macros.xbox_dpad_up()
+    time.sleep(DELAY)
+
+    # Navigates to "Trade"
+    Macros.xbox_a()
+    time.sleep(1)
+
+    # Ticks "Trade" button since the trade includes high rarity Sigils
+    Macros.xbox_a()
+    time.sleep(DELAY)
+
+    # Navigates to unlocked "Trade" button
+    Macros.xbox_dpad_down()
+    time.sleep(DELAY)
+
+    # Confirms action
+    Macros.xbox_a()
+    time.sleep(DELAY)
+
+    # Exits out of "Thank You!"
+    Macros.xbox_a()
+    time.sleep(0.5)
+
+    # Goes back to Knickknack Vouchers menu
+    Macros.xbox_b()
+    time.sleep(DELAY)
+
+    # Goes back to Main screen (Siero's Knickknack Shack)
+    Macros.xbox_b()
+    time.sleep(DELAY)
+
+    # Navigates to "Transmute Sigils"
+    Macros.xbox_dpad_down()
+    time.sleep(DELAY)
+
+    # Confirms navigation
+    Macros.xbox_a()
+    time.sleep(DELAY)
+
+    # At this point, the process starts again until the user desires to
+    # end the script
 
 def main():
     paused = False
-    DELAY = 0.25
 
     def pause_listener(_):
         nonlocal paused
@@ -35,83 +113,8 @@ def main():
             if not paused:
                 Macros.xbox_a()
 
-        # User navigates back to transmute sigils screen
-        while not is_on_screen(constants.SELECT_TRANSMUTATION, c=0.99):
-            Macros.xbox_a()
-            time.sleep(0.5)
-
-        time.sleep(1)
-
-        # Back to main navigation
-        Macros.xbox_b()
-        time.sleep(DELAY)
-
-        # Highlights "Knickknack Vouchers"
-        Macros.xbox_dpad_up()
-        time.sleep(DELAY)
-
-        # Navigates into "Knickknack Vouchers"
-        Macros.xbox_a()
-        time.sleep(DELAY)
-
-        # Navigates into "Trade Sigils"
-        Macros.xbox_a()
-        time.sleep(DELAY)
-
-        # Opens "Sort & Filter/Trade All"
-        Macros.xbox_back()
-        time.sleep(DELAY)
-
-        # "Trade All Using Specified Criteria"
-        Macros.xbox_x()
-        time.sleep(1)
-
-        if not is_on_screen(constants.TRADE_ALL):
-            print("You have no more sigils to trade for")
-            os._exit(0)
-
-        # Moves from "Cancel" to "Trade"
-        Macros.xbox_dpad_up()
-        time.sleep(DELAY)
-
-        # Navigates to "Trade"
-        Macros.xbox_a()
-        time.sleep(1)
-
-        # Ticks "Trade" button since the trade includes high rarity Sigils
-        Macros.xbox_a()
-        time.sleep(DELAY)
-
-        # Navigates to unlocked "Trade" button
-        Macros.xbox_dpad_down()
-        time.sleep(DELAY)
-
-        # Confirms action
-        Macros.xbox_a()
-        time.sleep(DELAY)
-
-        # Exits out of "Thank You!"
-        Macros.xbox_a()
-        time.sleep(0.5)
-
-        # Goes back to Knickknack Vouchers menu
-        Macros.xbox_b()
-        time.sleep(DELAY)
-
-        # Goes back to Main screen (Siero's Knickknack Shack)
-        Macros.xbox_b()
-        time.sleep(DELAY)
-
-        # Navigates to "Transmute Sigils"
-        Macros.xbox_dpad_down()
-        time.sleep(DELAY)
-
-        # Confirms navigation
-        Macros.xbox_a()
-        time.sleep(DELAY)
-
-        # At this point, the process starts again until the user desires to
-        # end the script
+        sell_sigils()
+        
 
 
 if __name__ == "__main__":
