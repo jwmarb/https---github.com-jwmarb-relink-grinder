58a534b3ba10906f0d73876486a4e1aeba4f2d2d
Joseph Marbella <dieptuber@gmail.com>
2024-04-01 19:57:44 -0700

    refactor: keybind mapping

---
+++ src/keybinds.py
@@ -0,0 +1,19 @@
+from xbox_layout import *
+
+# main buttons
+ATTACK              = X
+LINK_ATTACK         = B
+SPECIAL_ATTACK      = Y
+JUMP                = A
+
+# support buttons
+GUARD               = LEFT_SHOULDER
+AUTO_TARGET         = LEFT_TRIGGER
+SKILL_MENU          = RIGHT_SHOULDER
+DODGE               = RIGHT_TRIGGER
+
+# potion slots
+POTION_LARGE_GREEN  = DPAD_LEFT
+POTION_REVIVE       = DPAD_DOWN
+POTION_BLUE         = DPAD_RIGHT
+POTION_SMALL_GREEN  = DPAD_UP
